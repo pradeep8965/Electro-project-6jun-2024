66d91221100d56673213f9b1fdab5bc1f0952f3a
“misterpp143@gmail.comgit config --global user.email misterpp143@gmail.comgit config  credential.helper ‘store’git config --global user.name “misterpp143@gmail.com <misterpp143@gmail.com>
2024-06-19 17:45:11 +0530

    new

--- resources/views/components/aside.blade.php
+++ resources/views/components/aside.blade.php
@@ -1,8 +1,10 @@
+
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="/admin/dashboard" class="brand-link">
-        <img src="{{url('/')}}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{url('/')}}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -11,17 +13,19 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{url('/')}}/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="{{url('/')}}/dist/img/user2-160x160.jpg" class="img-circle elevation-2"
+                    alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Pradeep Prajapati</a>
+                <a href="#" class="d-block">Pradeep prajapati</a>
             </div>
         </div>
 
         <!-- SidebarSearch Form -->
         <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                    aria-label="Search">
                 <div class="input-group-append">
                     <button class="btn btn-sidebar">
                         <i class="fas fa-search fa-fw"></i>
@@ -32,7 +36,8 @@
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -51,7 +56,7 @@
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a class="nav-link">
                         <i class="nav-icon fas fa-heart"></i>
                         <p>
                             My Own Pages
@@ -59,12 +64,12 @@
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="/admin/general" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
+                       <li classes="nav-item">
+                            <a href="/admin/general" class ="nav-link"> 
+                                <i class="far fa-circle nav icon"> </i>
                                 <p>General</p>
                             </a>
-                        </li>
+                       </li>
                         <li class="nav-item">
                             <a href="/admin/icons" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
