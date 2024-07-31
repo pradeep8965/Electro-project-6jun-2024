4efba89e5450e45978397f7f7434f42fccf99049
“misterpp143@gmail.comgit config --global user.email misterpp143@gmail.comgit config  credential.helper ‘store’git config --global user.name “misterpp143@gmail.com <misterpp143@gmail.com>
2024-07-31 12:02:56 +0530

    new

--- app/Http/Controllers/ProductFilterController.php
+++ app/Http/Controllers/ProductFilterController.php
@@ -11,11 +11,24 @@ class ProductFilterController extends Controller
 
     //3. method
     public function filter(Request $request){
-        $products = DB::table('products')
-        ->join('brands', 'products.brand_id', '=', 'brands.id')
-        ->join('categories', 'products.category_id', '=', 'categories.category_id')
-        ->get();
-
+        /* dd($request->all()); */
+        
+        //Check if q prameter comming in url
+        
+        if($request->all()){
+            $q = $request->q;
+            //dd($q);
+            $products = DB::table('products')
+            ->join('brands', 'products.brand_id', '=', 'brands.id')
+            ->join('categories', 'products.category_id', '=', 'categories.category_id')
+            ->where('products.product_name', 'like', '%' . $q . '%')
+            ->get();
+        }else{
+            $products = DB::table('products')
+            ->join('brands', 'products.brand_id', '=', 'brands.id')
+            ->join('categories', 'products.category_id', '=', 'categories.category_id')
+            ->get();
+        }
         $brandProductCounts = DB::table('products')
         ->select('brands.brand_name', DB::raw('count(*) as productCount'))
         ->join('brands', 'products.brand_id', '=', 'brands.id')
